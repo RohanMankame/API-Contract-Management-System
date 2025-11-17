0b6ee4d30db7fb2de4a40b35ce5eb5e9d7e89eec
Rohan Mankame <rohanmankame@outlook.com>
2025-11-17 10:12:11 +0530

    new working branch, set up flask project

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "index page"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
