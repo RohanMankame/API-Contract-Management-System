25251073a5e74a0650142b3ea87234d877d0e001
Rohan Mankame <rohanmankame@outlook.com>
2025-11-18 15:03:36 +0530

    added endpoints for clients, other endpoints still to be fixed after DB change

--- app.py
+++ app.py
@@ -120,11 +120,61 @@ def getUserByID(id):
 
 #################|USER ENDPOINTS END|#####################
 
-#***********************|USER ENDPOINTS START|*********************#
+#***********************|Client ENDPOINTS START|*********************#
 
 
 
-#################|USER ENDPOINTS END|#####################
+#################|Client ENDPOINTS END|#####################
+
+@app.route('/createClient', methods=['POST'])
+def createClient():
+    '''
+    Create a new Client and save in DB
+    '''
+    if not request.is_json:
+        return {"error": "Invalid input, send user details in JSON format"}, 400
+    
+    data = request.get_json()
+    try:
+        client = Client(
+            username=data.get('username'),
+            email=data.get('email')
+        )
+        db.session.add(client)
+        db.session.commit()
+        return {"message": "Client created", "client_id": user.id}, 201
+    except Exception as e:
+        db.session.rollback()
+        return {"error": str(e)}, 400
+        
+
+@app.route('/getClients', methods=['GET'])
+def getClients():
+    '''
+    Get all Clients from DB
+    '''
+    try:
+        clients = Client.query.all()
+        clients_list = [{"id": client.id, "username": client.username, "email": client.email} for client in clients]
+        return {"client": clients_list}, 200
+    except Exception as e:
+        return {"error": str(e)}, 400
+
+
+@app.route('/getClient/<id>', methods=['GET'])
+def getClientByID(id):
+    '''
+    Get existing client from DB using user ID
+    '''
+    try:
+        client = Client.query.get(id)
+        if user:
+            return {"id": client.id, "username": client.username, "email": client.email}, 200
+        else:
+            return {"error": "User not found"}, 404
+    except Exception as e:
+        return {"error": str(e)}, 400
+
 
 
 #***********************|CONTRACT ENDPOINTS START|*********************#
