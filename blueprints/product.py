3778bab94db6c0d56c815fc473ee7aafce7a705f
Rohan Mankame <rohanmankame@outlook.com>
2025-12-01 11:35:04 +0530

    updated product endpoints to use schema

--- blueprints/product.py
+++ blueprints/product.py
@@ -2,15 +2,14 @@ from flask import Blueprint, request, jsonify
 from app import db
 from models import Product
 from flask_jwt_extended import jwt_required, get_jwt_identity   
+from schemas.product_schema import product_read_schema, products_read_schema, product_write_schema
+from marshmallow import ValidationError
 
 # Initialize product Blueprint
 product_bp = Blueprint('product', __name__)
 
 # Product Endpoints
 
-
-
-"""
 @product_bp.route('/Products', methods=['POST', 'GET'])
 @jwt_required()
 def Products():
@@ -18,120 +17,106 @@ def Products():
     Post: Create a new API product
     Get: Get all API products from DB
     '''
+    curr_user_id = get_jwt_identity()
+
     if request.method == 'POST':
         try:
-            curr_user_id = get_jwt_identity()
             data = request.get_json()
+            validated = product_write_schema.load(data)
 
-            new_product = Product(
-                api_name=data['api_name'],
-                description=data['description'],
-                is_archived=data.get('is_archived', False),
-                created_by=curr_user_id,
-                updated_by=curr_user_id
-                )
+            new_product = Product(**validated,created_by=curr_user_id,updated_by=curr_user_id)
 
             db.session.add(new_product)
             db.session.commit()
-            return jsonify({'message': 'Product created successfully', 'product_id': new_product.id}), 201
+
+            return jsonify(product=product_read_schema.dump(new_product)), 201
+            
+        except ValidationError as ve:
+            return jsonify({"error": ve.messages}), 400
 
         except Exception as e:
-            return jsonify({'message': 'Error creating product', 'error': str(e)}), 500
-        
+            return jsonify({"error": str(e)}), 400
+    
+
+
     elif request.method == 'GET':
         try:
             products = Product.query.all()
-            products_list = []
-
-            for product in products:
-                products_list.append({
-                    'id': product.id,
-                    'api_name': product.api_name,
-                    'description': product.description,
-                    'is_archived': product.is_archived,
-                    'created_at': product.created_at,
-                    'updated_at': product.updated_at
-                })
-
-            return jsonify({'products': products_list}), 200
+            return jsonify(products=products_read_schema.dump(products)), 200
+        
         except Exception as e:
-            return jsonify({'message': 'Error getting products', 'error': str(e)}), 500
-
-    return jsonify({'message': 'Method not allowed'}), 405
+            return jsonify({"error": str(e)}), 400
 
 
-@product_bp.route('/Products/<id>', methods=['GET','PUT','DELETE'])
+
+
+
+@product_bp.route('/Products/<id>', methods=['GET','PUT', 'PATCH','DELETE'])
 @jwt_required()
 def Product_id(id):
     ''' 
     Get: Get details of spefic Product(API)
-    Put: Update details of product with given ID
+    Put/PATCH: Update details of product with given ID
     Delete: Archive a product with given ID
     '''
+    curr_user_id = get_jwt_identity()
+
     if request.method == 'GET':
         try:
             product = Product.query.get(id)
             if not product:
                 return jsonify({'message': 'Product not found'}), 404
 
-            product_data = {
-                'id': product.id,
-                'api_name': product.api_name,
-                'description': product.description,
-                'is_archived': product.is_archived,
-                'created_at': product.created_at,
-                'updated_at': product.updated_at,
-                'created_by': product.created_by,
-                'updated_by': product.updated_by
-            }
-
-            return jsonify({'product': product_data}), 200
+            return jsonify(product=product_read_schema.dump(product)), 200
 
         except Exception as e:
             return jsonify({'message': 'Error getting product', 'error': str(e)}), 500
 
 
 
-    elif request.method == 'PUT':
+    elif request.method == 'PUT' or request.method == 'PATCH':
         try:
             data = request.get_json()
+            validated = product_write_schema.load(data, partial=True)
 
             product = Product.query.get(id)
             if not product:
                 return jsonify({'message': 'Product not found'}), 404
 
-            if 'api_name' in data:
-                product.api_name = data['api_name']
-            if 'description' in data:
-                product.description = data['description']
-            if 'is_archived' in data:
-                product.is_archived = data['is_archived']
-            product.updated_by = get_jwt_identity()
+            for key, value in validated.items():
+                setattr(product, key, value)
+            product.updated_by = curr_user_id
             db.session.commit()
 
             return jsonify({'message': 'Product updated successfully'}), 200
 
+        except ValidationError as ve:
+            return jsonify({"error": ve.messages}), 400
+
         except Exception as e:
             return jsonify({'message': 'Error updating product', 'error': str(e)}), 500
 
+            
+    
     elif request.method == 'DELETE':
         try:
             product = Product.query.get(id)
             if not product:
                 return jsonify({'message': 'Product not found'}), 404
-            
+
             product.is_archived = True
-            product.updated_by = get_jwt_identity()
+            product.updated_by = curr_user_id
 
             db.session.commit()
             return jsonify({'message': 'Product has been archived successfully'}), 200
-        
+
         except Exception as e:
             return jsonify({'message': 'Error archiving product', 'error': str(e)}), 500
 
-    return jsonify({'message': 'Method not allowed'}), 405
 
 
+
+"""
 
 @product_bp.route('/Products/<id>/Contracts', methods=['GET'])
 @jwt_required()
